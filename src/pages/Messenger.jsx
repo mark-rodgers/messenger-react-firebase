4e424a6d4e74e41bf988f7eb9c0d0541858d5e57
Mark Rodgers <mark@marknrodgers.com>
2023-02-05 00:07:44 -0800

    add example .env

--- src/pages/Messenger.jsx
+++ src/pages/Messenger.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 const Messenger = () => {
-  return <div>Messenger</div>;
+  const { currentUser } = useAuth();
+
+  return (
+    <div className="text-center">
+      <div className="text-2xl max-w-xs my-8 mx-auto">
+        Wow! This is the best messaging app I've ever seen!
+        <div className="text-right mx-4">- Mark</div>
+      </div>
+      <img src="/great-success.gif" className="w-full max-w-xs mx-auto my-8" />
+      <div className="text-left whitespace-pre-wrap break-all bg-slate-200 p-4 mx-4 my-8">
+        {JSON.stringify(currentUser, null, 2)}
+      </div>
+    </div>
+  );
 };
 
 export default Messenger;
